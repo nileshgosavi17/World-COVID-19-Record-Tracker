68cce34844323ead7bdafa1d310fffdf2081e094
nileshgosavi17 <400118nileshgosavi@mgmtech.org>
2021-07-18 11:20:34 +0530

    2nd commit

---
+++ src/components/Map.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Map.css";
+
+function Map() {
+  return (
+    <div className="map">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/TciahD5uVS0"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default Map;
